2332b8387983b55cca427cceedbf02203af562b5
andrescantero10 <andrescantero10@gmail.com>
2024-01-24 11:00:19 -0700

    postman usage and redis implementation. app.post function implemented

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const Redis = require('redis'); //Import Redis Class from the Library
+const bodyParser = require('body-parser');
 
-const app = express();
-app.listen(3000);
+//import express from 'express'
+const redisClient = Redis.createClient({
+    url:`redis://localhost:6379`
+});
+const app = express(); //creates an express application
+const port = 3000; //port #
+
+app.use(bodyParser.json()); // use bodyParser
+
+app.listen(port,()=>{
+    console.log(`API is Listening on port: ${port}`);
+    redisClient.connect(); // this connects the redis database!
+}); //listens for web requests from the frontend and don't stop
 
 
-const boxes = [
-    {boxId:1},
-    {boxId:2},
-    {boxId:3},
-    {boxId:4},
-];
-app.get('/boxes', (req,res)=>{
-    res.send(JSON.stringify(boxes));
+app.get('/boxes',async (req,res)=>{
+    let boxes = await redisClient.json.get('boxes',{path:'$'});// get the boxes
+    //send the boxes to the browser
+    res.json(boxes);//convert boxes to a JSON string
 });
 
+app.post('/boxes', async (req, res) => {
+    const newBox = req.body;
+    newBox.id =  parseInt(await redisClient.json.arrLen('boxex','$'))+1; // id set on user prompt
+    await redisClient.json.arrAppend('boxes', '$', newBox); // saves the JSON in redis
+    res.json(newBox); //respond with something, in this case, the newBox
+
+ });
 
 console.log("hello");
